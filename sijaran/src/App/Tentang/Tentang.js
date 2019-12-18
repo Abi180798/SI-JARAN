4b7e1aa66c5af5e48611af7e3ba7c3d944213b30
rbayuk <bayupkpr@gmail.com>
2019-12-19 07:26:18 +0800

    tentang

--- sijaran/src/App/Tentang/Tentang.js
+++ sijaran/src/App/Tentang/Tentang.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Layout, Card } from 'antd'
+import { Layout, Card, Table } from 'antd'
 import { Switch, Route } from 'react-router-dom'
 import Header from '../../Components/Header'
 import Footer from '../../Components/Footer'
@@ -22,38 +22,35 @@ const Tentang = () => {
                   <Typography>
                     <Title>Pendahuluan</Title>
                     <Paragraph>
-                      <text strong > <b>Universitas Mataram memiliki kendaraan umum untuk digunakan  demi kebutuhan umum masyarakat Universitas.
-                        </b></text>
+                      <h3><text>Universitas Mataram memiliki kendaraan umum untuk digunakan demi kebutuhan umum warga Universitas Mataram. Kendaraan ini dapat digunakan untuk warga Universitas Mataram dengan ketentuan yang terlampir pada sistematis kepengurusan di Rektorat Universitas Mataram. Tentunya terdapat prasyarat untuk dapat memperoleh kendaraan tersebut sebagaimana terdapat pada prosedur peminjaman yang terlampir.</text> </h3>
                       <div align="center">
                         <img src="http://skifkunram.com/site/wp-content/uploads/2017/04/DSC_2747-Small.jpg"
                           style={{
-                            width: 500,
+                            width: 700,
                           }}></img>
                       </div>
-                      Adapun prosedur yang harus dilakukan dalam peminjaman kendaraan meliputi :
+                      <h2><b>Adapun prosedur yang harus dilakukan dalam peminjaman kendaraan meliputi :</b></h2>
                       <ul>
                         <li>
-                          <b>Membuat surat pengajuan peminjaman kendaraan ke Rektorat Unram</b>
+                          <h3>Membuat surat pengajuan peminjaman kendaraan ke Rektorat Unram</h3>
                         </li>
                         <li>
-                          <b>Isi surat harus jelas sepeti contoh yang dilampirkan</b>
+                          <h3>Isi surat harus jelas sepeti contoh yang dilampirkan</h3>
                           <div align="center">
-
                             <img src={peminjaman} style={{
-                              width: 300,
+                              width: 500,
                             }}></img>
                           </div>
+                          <text>Bagian dari pembentukan sudat terdapat: Kopsurat asal fakultas yang diutus, Waktu, Nomor Surat, Perihal pengajuan surat, Utusan surat, Salam pembuka, Isi surat (lengkap dengan waktu peminjaman), Salam penutup, Tertera untuk Ketua kegiatan, Sekertaris Kegiatan, dan Mengetahui Dekan Fakultas  </text>
                         </li>
                         <li>
-                          <b>Sudah diketahui oleh pihak Rektorat</b>
+                          <h3>Sampaikan ke pada protokol terkait peminjaman kendaraan dan menunggu pengarahan</h3>
                         </li>
                         <li>
-                          <b>Menunggu konfirmasi selanjutnya dengan pencocokan data kendaraan</b>
+                          <h3>Menunggu konfirmasi selanjutnya dengan pencocokan data kendaraan</h3>
                         </li>
                       </ul>
                     </Paragraph>
-
-
 
 
                   </Typography>
