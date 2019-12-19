bbe83422a9993fc2f6f8b4d068510a0974031f6e
RagilAM <ragilabdimaulana@yahoo.co.id>
2019-12-19 11:21:57 +0800

    ui login

--- sijaran/src/App/Auth/Login.js
+++ sijaran/src/App/Auth/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Form, Input, Button, Row, Col, Spin } from 'antd'
+import { Card, Form, Input, Button, Row, Col, Spin, Icon } from 'antd'
 import { withFormik, } from 'formik'
 import * as Yup from 'yup'
 import fconfig from '../../config/fconfig'
@@ -27,46 +27,47 @@ const Login = ({
     return <Redirect to='/dashboardadmin'></Redirect>
   }
   return (
-    <Row style={{
-      marginTop: 150
-    }}>
-      <Col span={8} offset={8}>
-        <Card>
-          <Form onSubmit={handleSubmit}>
-            <h1>Login</h1>
-            <Form.Item label="Email">
-              <Input
-                type="email"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-                name="email"
-                autoComplete="off"
-                placeholder="Masukkan Email"
-              />
-              {errors.email && touched.email && <div>{errors.email}</div>}
-            </Form.Item>
-            <Form.Item label="Password">
-              <Input
-                type="password"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.password}
-                name="password"
-                placeholder="Masukkan Password"
-              />
-              {errors.password && touched.password && <div>{errors.password}</div>}
-            </Form.Item>
-            <Button htmlType='button' style={{
-              float: "right"
-            }}><Link to="/">Kembali</Link></Button>
-            <Button htmlType="submit">Submit</Button>
+    <div style={{ background: '#bdc3c7', paddingTop: 150, paddingBottom: 150 }}>
+      <Row>
+        <Col span={8} offset={8}>
+          <Card>
+            <Form onSubmit={handleSubmit}>
+              <h1>Login</h1>
+              <Form.Item label="Email">
+                <Input
+                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  type="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                  name="email"
+                  autoComplete="off"
+                  placeholder="Masukkan Email"
+                />
+                {errors.email && touched.email && <div>{errors.email}</div>}
+              </Form.Item>
+              <Form.Item label="Password">
+                <Input
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  type="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                  name="password"
+                  placeholder="Masukkan Password"
+                />
+                {errors.password && touched.password && <div>{errors.password}</div>}
+              </Form.Item>
+              <Button htmlType='button' style={{
+                float: "right"
+              }}><Link to="/">Kembali</Link></Button>
+              <Button htmlType="submit">Submit</Button>
 
-          </Form>
-        </Card>
-      </Col>
-    </Row>
-
+            </Form>
+          </Card>
+        </Col>
+      </Row>
+    </div>
   );
 };
 const FormikApp = withFormik({
